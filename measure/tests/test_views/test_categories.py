b48c362f8e096bc3ecbae0753053a67595269be7
Michał Piotr Stankiewicz <stinger.dat@gmail.com>
2016-10-12 16:34:02 +0200

    feat(measure/views.py...): views, serializer, test, etc. for categories query

---
+++ measure/tests/test_views/test_categories.py
@@ -0,0 +1,32 @@
+import json
+
+from django.core.urlresolvers import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from ...models import Category
+
+
+class CategoriesTestCase(APITestCase):
+    def test_empty_db_empty_response(self):
+        url = reverse('measure:category-list')
+        response = self.client.get(url, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertFalse(json.loads(response.content.decode('utf-8')))
+
+    def test_some_objects_in_db_some_in_response(self):
+        Category.objects.create(name='weight')
+        Category.objects.create(name='height')
+
+        url = reverse('measure:category-list')
+        response = self.client.get(url, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertListEqual(
+            json.loads(response.content.decode('utf-8')),
+            [
+                {'name': 'weight'},
+                {'name': 'height'},
+            ]
+        )
