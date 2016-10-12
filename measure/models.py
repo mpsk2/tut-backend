2d139471aef2c9e481923bb4ccdb9fbf95bf7f6d
Michał Piotr Stankiewicz <stinger.dat@gmail.com>
2016-10-12 15:38:51 +0200

    feat(measure/models.py): add basic model for measures

--- measure/models.py
+++ measure/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    records = models.ManyToManyField(User, through='Record')
+
+
+class Record(models.Model):
+    user = models.ForeignKey(User)
+    category = models.ForeignKey(Category)
+    value = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    taken_at = models.DateTimeField()
